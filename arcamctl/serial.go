1087f61c18ed84f84df505dfba8798e7b6532adf
Mike Pountney <Mike.Pountney@gmail.com>
2020-05-10 06:34:31 +0100

    Add a reader, so we can parse the return messages from the amp
    Have a singleton reader looking at messages coming from the amp, as
    these can come in independently from our commands being issued. eg, from
    volume changes from the remote.
    
    This is kinda neat, being able to have a stream of status to parse and
    use to update some kind of internal state

--- arcamctl/serial.go
+++ arcamctl/serial.go
@@ -3,6 +3,7 @@ package arcamctl
 import (
 	"github.com/tarm/serial"
 	"log"
+	"strings"
 )
 
 const SERIAL_DEV = "/dev/ttyUSB0"
@@ -22,15 +23,88 @@ func init() {
 	go reader(s)
 }
 
+func handleStatusMessages(msgs []string) {
+	for _, msg := range msgs {
+		HandleStatusMessage(msg)
+	}
+}
+
+/*
+*  AV_0 - volume set
+*  AV_* - power
+*  AV_. - mute
+*  AV_/ - volume inc/dec/status
+*  AV_1 - source select
+ */
+func HandleStatusMessage(msg string) {
+	if len(msg) < 4 {
+		log.Printf("message too short: %v\n", msg)
+		return
+	}
+
+	family := msg[:4]
+	switch family {
+	case "AV_0":
+		handleVolumeSetStatus(msg)
+	case "AV_/":
+		handleVolumeStatus(msg)
+	case "AV_*":
+		handlePowerStatus(msg)
+	case "AV_.":
+		handleMuteStatus(msg)
+	case "AV_1":
+		handleSourceStatus(msg)
+	default:
+		log.Printf("unhandled message family: %s\n", family)
+	}
+}
+
+func handleVolumeSetStatus(msg string) {
+	log.Printf("VolumeSetStatus: %s", msg)
+}
+
+func handleVolumeStatus(msg string) {
+	log.Printf("VolumeStatus: %s", msg)
+}
+
+func handlePowerStatus(msg string) {
+	log.Printf("PowerStatus: %s", msg)
+}
+
+func handleMuteStatus(msg string) {
+	log.Printf("MuteStatus: %s", msg)
+}
+
+func handleSourceStatus(msg string) {
+	log.Printf("SourceStatus: %s", msg)
+}
+
 func reader(s *serial.Port) {
 	log.Println("Setting up port reader")
+	var msgOverrun []byte
 	for {
+
 		buf := make([]byte, 128)
 		n, err := s.Read(buf)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("%q", buf[:n])
+
+		totalMsg := string(msgOverrun) + string(buf[:n])
+		msgs := strings.Split(totalMsg, "\r")
+
+		if msgs[len(msgs)-1] == "" {
+			msgOverrun = []byte(``)
+		} else {
+			// end of buffer was not a complete message terminated with \r
+			msgOverrun = []byte(msgs[len(msgs)-1])
+		}
+
+		// last element is either empty or an incomplete buffer, strip it
+		msgs = msgs[:len(msgs)-1]
+
+		handleStatusMessages(msgs)
+
 	}
 }
 
